0841a4351da922f57ba2b943bedde247fd9f9543
Bash- <Bash-@users.noreply.github.com>
2024-07-04 00:35:30 +0200

    Adapted frontpage layout

--- frontend/components/empty-screen.tsx
+++ frontend/components/empty-screen.tsx
@@ -19,7 +19,8 @@ export function EmptyScreen({ emptyScreenHeader, emptyScreenBody }: { emptyScree
           <>
             <p className="leading-normal text-muted-foreground">
               Dit is een open source chatbot dat vragen kan beantwoorden over aanbestedingen van het Rijk.
-              Stel gerust een vraag of probeer een van de voorbeelden hieronder.
+              {/* Stel gerust een vraag of probeer een van de voorbeelden hieronder. */}
+              Klik hieronder om relevante Tenders en Rijksdocumenten te bekijken en vragen over te stellen.
             </p>
             <p className="leading-normal text-muted-foreground">
               Het AI model is getraind op een dataset met actuele aanbestedingsdata van het Rijk,
