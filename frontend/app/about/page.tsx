c9e3dc12e4231febfb6e00967264e22d3d95b9d1
Bas Hendrikse <Bash-@users.noreply.github.com>
2024-09-12 22:55:08 +0200

    Updated about

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -14,6 +14,14 @@ export default async function AboutPage() {
       <p className="leading-normal text-muted-foreground">
         Dit is een open source Tenderportaal dat met behulp van AI vragen kan beantwoorden over aanbestedingen van het Rijk.
       </p>
+
+      <p>
+        Vragen of opmerkingen? Neem contact op via <a className="underline" href="mailto:info@tenderflow.eu">info@tenderflow.eu</a>
+      </p>
+      <p className="leading-normal text-muted-foreground">
+        Ook als u geinteresseerd bent in het gebruik van deze technologie voor uw eigen organisatie, dan horen we graag van u.
+      </p>
+      
       <br></br>
       <p className="leading-normal text-muted-foreground">
         Deze webapplicatie wordt ontwikkeld naar aanleiding van de <Link className="underline" href="https://www.rijksoverheid.nl/onderwerpen/zakendoen-met-het-rijk/kick-off-open-inkoopdata-app-challenge"> Open Inkoopdata App Challenge</Link>.
@@ -39,6 +47,7 @@ export default async function AboutPage() {
           <li> - Aankondigingen van Tenderned en bijbehorende documenten </li>
           <li> - Categorieplannen van de Rijksoverheid </li>
           <li> - Rijksvoorwaarden </li>
+          <li> - De website van PIANOo Expertisecentrum Aanbesteden </li>
         </ul>
       </p>
       <br></br>
@@ -47,16 +56,7 @@ export default async function AboutPage() {
         De vragen worden met behulp van Retrieval Augmented Generation (RAG) beantwoord.
         Ons programma zoekt in de documenten naar relevante stukken tekst, dit wordt samen met de vraag naar de OpenAI API gestuurd (van dezelfde maker als ChatGPT).
         De OpenAI API genereert een antwoord en stuurt dit terug naar de gebruiker.
-      </p>
-
-      <br></br>
-      <p>
-        Vragen of opmerkingen? Neem contact op via <a className="underline" href="mailto:info@tenderflow.eu">info@tenderflow.eu</a>
-      </p>
-      <p className="leading-normal text-muted-foreground">
-        Ook als u geinteresseerd bent in het gebruik van deze technologie voor uw eigen organisatie, dan horen we graag van u.
-      </p>
-
+      </p>    
     </div>
   )
 }
